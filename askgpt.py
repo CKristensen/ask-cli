b9ec460564c324848fcee10ad99d1ed653e61936
Carl Kristensen <carl.johan.coelho.kristensen@schibsted.com>
2023-02-17 22:08:08 +0100

    added the option to ask wolfram alpha

--- askgpt.py
+++ askgpt.py
@@ -2,16 +2,24 @@
 import openai
 import os
 import argparse
+import requests
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
+wolfram_app_id = os.getenv('WOLFRAM_APP_ID')
 
-parser = argparse.ArgumentParser(prog='Ask GPT from CLI!',
+parser = argparse.ArgumentParser(prog='Ask AI from CLI. Ask GPT from the comfort of your CLI.',
                                  epilog='ask -m cd "make a python script that print hello world!"')
 parser.add_argument('default')
-parser.add_argument("-m,", "--model", help="""
-available models: td: text-davinci-003 || tc: text-curie-001 || tb: text-babbage-001 || ta: text-ada-001 || cd: code-davinci-002 || cc: code-cushman-001
-""")
-parser.add_argument("-v,", "--verbose", action='store_true')
+parser.add_argument("-m,", "--model", help="""available models:
+                    td: text-davinci-003    ||
+                    tc: text-curie-001      ||
+                    tb: text-babbage-001    ||
+                    ta: text-ada-001        ||
+                    cd: code-davinci-002    ||
+                    cc: code-cushman-001    ||
+                    w: wolfram-alpha-simple ||""")
+parser.add_argument("-t", "--temperature", help="""What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.""")
+parser.add_argument("-v", "--verbose", action='store_true')
 
 model_dict = {
     "td": "text-davinci-003",
@@ -20,6 +28,7 @@ model_dict = {
     "ta": "text-ada-001",
     "cd": "code-davinci-002",
     "cc": "code-cushman-001",
+    "w": "wolfram-alpha",
     "text-davinci-003": "text-davinci-003",
     "text-curie-001": "text-curie-001",
     "text-babbage-001": "text-babbage-001",
@@ -29,28 +38,45 @@ model_dict = {
 }
 args = parser.parse_args()
 model = model_dict.get(args.model or "text-davinci-003") 
+temperature = args.temperature or 1
 
 def ask_chatgpt(prompt):
-  max_tokens = 4000 if "davinci" in model else 2048
-  requested_tokens = max_tokens - int(len(prompt)/2)
-  completions = openai.Completion.create(
-  engine=model,
-  prompt=prompt,
-  max_tokens=requested_tokens,
-  n=1,
-  stop=None,
-  temperature=0.5,
-  )
+    try:
+      max_tokens = 4000 if "davinci" in model else 2048
+      requested_tokens = max_tokens - int(len(prompt)/2)
+      completions = openai.Completion.create(
+      engine=model,
+      prompt=prompt,
+      max_tokens=requested_tokens,
+      n=1,
+      stop=None,
+      temperature=temperature,
+      )
+    
+      message = completions.choices[0].text
+      return message
+    except openai.error.APIError as e:
+        response = str(e)
+    except openai.error.RateLimitError as e:
+        response = str(e)
 
-  message = completions.choices[0].text
-  return message
+def ask_wolfram(question):
+    # Make a request to the Wolfram Alpha API
+    r = requests.get('http://api.wolframalpha.com/v1/spoken',
+                     params={'output': 'json', 'input': question, 'appid': wolfram_app_id,
+                             'format': 'plaintext'})
+    if r.status_code == 200:
+        print(str(r.text))
+    else:
+        # Return an error message
+        return f'I could not get answer from Wolfram Alpha. {r.status_code}'
+
 if args.verbose:
     print(f"asking {model}")
     print(f"asking {args.default}")
-try:
+if model != "wolfram-alpha":
     response = ask_chatgpt(args.default)
-except openai.error.APIError as e:
-    response = str(e)
-except openai.error.RateLimitError as e:
-    response = str(e)
-print(response)
+else:
+    response = ask_wolfram(args.default)    
+
+print(response)                             
